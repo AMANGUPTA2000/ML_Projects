29d892f9ccbf1a5d4020f823f068d60180aa4700
Aman Gupta <aman.180410116028@gmail.com>
2020-05-22 18:29:12 +0530

    remove

--- hey.py
+++
@@ -1,6 +0,0 @@
-# take input from the user
-celsius = float(input('Enter degree Celsius: '))
-
-# calculate fahrenheit
-fahrenheit = (celsius * 1.8) + 32
-print('%0.1f degree Celsius =  %0.1f degree Fahrenheit' %(celsius,fahrenheit))
